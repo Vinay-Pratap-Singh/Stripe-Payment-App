af5e06cad5ae604df132ac32b033831d12b2fc1d
Vinay Pratap Singh <technicalharvi@gmail.com>
2023-07-21 11:13:12 +0530

    feat: created server and connected to db

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,22 @@
-// backend/index.js
-const express = require("express");
+import "dotenv/config";
+import express from "express";
+import mongoose from "mongoose";
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, Backend!");
+// mongodb connection
+const dbURL = process.env.MONGODB_URI || "mongodb://localhost:27017/mydatabase";
+mongoose.connect(dbURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB successfully!");
 });
 
+// starting the server
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running at port https://localhost:${port}`);
 });
